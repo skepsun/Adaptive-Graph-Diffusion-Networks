2c34d764c9e6b088bd7daee271abfa8d6a9420e3
skepsun <chuxiongsun@gmail.com>
2020-12-15 22:59:50 +0800

    Update gat-ha.py

--- src/gat-ha.py
+++ src/gat-ha.py
@@ -70,11 +70,11 @@ def compute_acc(pred, labels, evaluator):
 def seed(seed=0):
     random.seed(seed)
     np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    th.manual_seed(seed)
+    th.cuda.manual_seed(seed)
+    th.cuda.manual_seed_all(seed)
+    th.backends.cudnn.deterministic = True
+    th.backends.cudnn.benchmark = False
     dgl.random.seed(seed)
 
 def add_labels(feat, labels, idx):
